d32e32bab469e92529dd3a76f10ff18d59b984b0
Jessica Moore <jessica.cmoore1@gmail.com>
2019-05-10 17:05:44 -0400

    Updated deliver method to include bonus for going over quota

--- paperboy.py
+++ paperboy.py
@@ -13,16 +13,24 @@ class Paperboy:
 
   def deliver(self, start_address, end_address):
     papers_delivered = abs(end_address - start_address)
-    money_earned = papers_delivered * .25
     self.experience += papers_delivered
-    self.earnings += money_earned
+
     if papers_delivered < self.quota:
-      self.increase_quota()
       self.earnings -= 2
-      return "You only delivered {} papers, and have been penalized $2.00. You've earned ${:.2f} on this delivery".format(papers_delivered, money_earned - 2)
-    self.increase_quota()
-    return "Well done! You've delivered {} papers and earned ${:.2f} on this delivery!".format(papers_delivered, money_earned)
-  
+
+      for paper in range(papers_delivered):
+        self.earnings += 0.25 
+      self.increase_quota()      
+      return "You only delivered {} papers, and have been penalized $2.00.".format(papers_delivered)
+      
+    elif papers_delivered >= self.quota:
+      for paper in range(papers_delivered):
+        self.earnings += 0.25 
+      for paper in range(papers_delivered - self.quota):
+        self.earnings += 0.25 
+      self.increase_quota() 
+      return "Well done! You've delivered {} papers!".format(papers_delivered)     
+
   def report(self):
     return f"I'm {self.name}, I've delivered {self.experience} papers so far!"
 
@@ -30,10 +38,11 @@ class Paperboy:
 paperboy1 = Paperboy('Johnny')
 
 print(paperboy1)
-print(paperboy1.deliver(680, 650))
+print('-------------------------')
+print(paperboy1.deliver(680, 600))
 print(paperboy1.report())
 print(paperboy1)
 print('-------------------------')
-print(paperboy1.deliver(680, 800))
+print(paperboy1.deliver(680, 670))
 print(paperboy1.report())
 print(paperboy1)
